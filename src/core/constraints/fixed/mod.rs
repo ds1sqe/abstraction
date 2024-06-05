01e52cba8972f5a48de14a97cffbf0104065eb77
ds1sqe <ds1sqe@mensakorea.org>
2024-06-05 17:32:13 +0900

    add fixed constraint check

--- src/core/constraints/fixed/mod.rs
+++ src/core/constraints/fixed/mod.rs
@@ -1,5 +1,24 @@
 /// represent id have to equal to value
-struct Fixed<V: PartialEq> {
+
+#[derive(Clone, Copy)]
+struct Fixed<V: PartialEq + Copy> {
     id: usize,
     value: V,
+}
+
+pub enum FixedCheckResult<V: PartialEq + Copy> {
+    Ok,
+    NotEqual { fixed: Fixed<V>, value: V },
+}
+
+impl<V: PartialEq + Copy> Fixed<V> {
+    pub fn is_in(&self, val: &V) -> FixedCheckResult<V> {
+        match self.value.eq(val) {
+            true => FixedCheckResult::Ok,
+            false => FixedCheckResult::NotEqual {
+                fixed: *self,
+                value: *val,
+            },
+        }
+    }
 }
